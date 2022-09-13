8036fe28173f4964018ddba94fdd8f15058e1853
Dana <dana@hlavacova.de>
2022-09-13 13:49:18 +0200

    funguje hashing password

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/registration', name: 'app_registration')]
+    public function index(Request $request,EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $data = [];
+        $form = $this->createFormBuilder($data)
+            ->add('email',TextType::class,['label'=>'Email', 'trim'=>true])
+            ->add('password', PasswordType::class,['label'=>'Zadej svuj pasword'])
+            ->add('submit', SubmitType::class, ['label' => 'Uloz'])
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $user = new User();
+            $email = $data['email'];
+            $plaintextPassword = $data['password'];
+
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hashedPassword);
+            $user->setEmail($email);
+
+            $em->persist($user); // nachystej - preklad pokracuj
+            $em->flush($user);
+            $this->addFlash("success", "Uzivatel byl pridan");
+
+            return $this->redirectToRoute('app_tydenni_jidelnicek');
+        }
+        return $this->renderForm('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
+            'form' => $form,
+        ]);
+    }
+}
