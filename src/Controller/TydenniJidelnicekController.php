805b39f7adb2d0556d7ea5ab84de0788baf2e9a0
Dana <dana@hlavacova.de>
2022-09-27 08:40:50 +0200

    Alle Tage der Woche in Speiseliste - Demoversion

--- src/Controller/TydenniJidelnicekController.php
+++ src/Controller/TydenniJidelnicekController.php
@@ -22,8 +22,11 @@ class TydenniJidelnicekController extends AbstractController
         $vybraneJidlo = new NazevJidla();
 
 
-        $form = $this->createFormBuilder($vybraneJidlo)
-            ->add('montag', EntityType::class,
+        $form = $this->createFormBuilder($vybraneJidlo);
+        $tage=['Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag','Sonntag'];
+        foreach($tage as $tag){
+
+            $form->add('fruhstuck' . $tag, EntityType::class,
                 [
                     'class' => NazevJidla::class,
                     'choice_label' => 'nazev',
@@ -35,61 +38,53 @@ class TydenniJidelnicekController extends AbstractController
                 ]
             )
 
-            ->add('utery', EntityType::class,
-                [
-                    'class' => NazevJidla::class,
-                    'choice_label' => 'nazev',
-                    'label' => 'Snack',
-                    'mapped' => false,
-                    'multiple' => false,
-                    'expanded' => false,
-                    'placeholder' => '',
-                ]
-            )
-            ->add('streda', EntityType::class,
-                [
-                    'class' => NazevJidla::class,
-                    'choice_label' => 'nazev',
-                    'label' => 'Mittagessen',
-                    'mapped' => false,
-                    'multiple' => false,
-                    'expanded' => false,
-                    'placeholder' => '',
-                ]
-            )
-            ->add('ctvrtek', EntityType::class,
-                [
-                    'class' => NazevJidla::class,
-                    'choice_label' => 'nazev',
-                    'label' => 'Abendsbrot',
-                    'mapped' => false,
-                    'multiple' => false,
-                    'expanded' => false,
-                    'placeholder' => '',
-                ]
-            )
-//            ->add('patek', EntityType::class,
-//                [
-//                    'class' => NazevJidla::class,
-//                    'choice_label' => 'nazev',
-//                    'label' => 'Patek',
-//                    'mapped' => false,
-//                    'multiple' => false,
-//                    'expanded' => false,
-//                    'placeholder' => '',
-//                ]
-//            )
-            ->add('submit', SubmitType::class, ['label' => 'Speichern'])
-            ->getForm();
+                ->add('snack' . $tag, EntityType::class,
+                    [
+                        'class' => NazevJidla::class,
+                        'choice_label' => 'nazev',
+                        'label' => 'Snack',
+                        'mapped' => false,
+                        'multiple' => false,
+                        'expanded' => false,
+                        'placeholder' => '',
+                    ]
+                )
+                ->add('mittagessen' . $tag, EntityType::class,
+                    [
+                        'class' => NazevJidla::class,
+                        'choice_label' => 'nazev',
+                        'label' => 'Mittagessen',
+                        'mapped' => false,
+                        'multiple' => false,
+                        'expanded' => false,
+                        'placeholder' => '',
+                    ]
+                )
+                ->add('abendsbrot' . $tag, EntityType::class,
+                    [
+                        'class' => NazevJidla::class,
+                        'choice_label' => 'nazev',
+                        'label' => 'Abendsbrot',
+                        'mapped' => false,
+                        'multiple' => false,
+                        'expanded' => false,
+                        'placeholder' => '',
+                    ]
+                );
+        }
+
+            $form->add('submit', SubmitType::class, ['label' => 'Speichern']);
 
 
         $montag = date('d.m.Y', strtotime('sunday next week'));
         $sonntag = date('d.m.Y', strtotime('sunday next week'));
 
         return $this->renderForm('tydenni_jidelnicek/index.html.twig', [
-           'form' => $form,
+
+           'form' => $form->getForm(),
             'montag' => $montag,
             'sonntag' => $sonntag,
+            'tage' => $tage
 
         ]);
     }
